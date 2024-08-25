dc1875f2695f6177b169cfc99684bf777b9f36ba
Hari96 <bob@rgbassociates.co.uk>
2024-08-25 18:41:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ output.style.textAlign = 'center';
 btn.textContent = "START GAME!";
 output.textContent = "Words Loading..... ";
 output.style.letterSpacing = '0';
+output.style.fontSize = "1.3em";
+
 /// Add to HTML page
 gameArea.append(output);
 gameArea.append(inWord);
-gameArea.prepend(scoreBoard);
+gameArea.append(scoreBoard);
 gameArea.append(selWordList);
 gameArea.append(btn);
 
@@ -34,14 +36,14 @@ selWordList.style.display = 'none';
 let theWords = [];
 let myWords = [];
 const game = { sel: '', scramble: '', score: 0, incorrect: 0, wordsLeft: 0, played: myWords.length, inplay: false };
-
+let tempWord = "";
 let url = 'https://docs.google.com/spreadsheets/d/1j73pDPxGyVnL_W0fV8WyKsp_AKo2xX4D4YZwRptRgh0/gviz/tq?';
 
 fetch(url)
     .then(res => res.text())
     .then(rep => {
         const data = JSON.parse(rep.substring(47).slice(0, -2));
-        console.log(data);        
+        //console.log(data);        
         data.table.rows.forEach((row, index) => {
             let holder = [];
             let opt = document.createElement('option');
@@ -49,7 +51,7 @@ fetch(url)
             opt.value = index;
             selWordList.append(opt);
             row.c.forEach((item) => {
-                console.log(item.v);
+                //console.log(item.v);
                 holder.push(item.v);
             });
             holder.shift();
@@ -59,22 +61,33 @@ fetch(url)
         btn.style.display = 'block';
         output.textContent = "Please Select your animal word list";
         selWordList.style.display = 'block';
-        console.log(theWords);
+        //console.log(theWords);
 })
 
 /// event listeners
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', (e) => {    
     if (!game.inplay) {
         selWordList.style.display = 'none';
-        myWords = theWords[selWordList.value];
+        myWords = theWords[selWordList.value].join(' ').split(' ');
+        //myWords = ["wow", "testing"];
+        game.played = myWords.length;
+        game.inplay = true;
     }
-    //console.log(myWords);
+    ////console.log(myWords);
     if (myWords.length <= 0) {
-        gameArea.innerHTML = `<div>GAME OVER</div>`;
-        gameArea.innerHTML = `<div>You got ${game.score} correct vs ${game.incorrect} incorrect out of ${game.played} words total.</div>`;
+        game.inplay = false;
+        scoreBoard.style.display = 'none';
+        output.style.fontSize = '1.2em';
+        output.style.letterSpacing = '0';
+        output.innerHTML = `<div>GAME OVER</div>`;
+        output.innerHTML = `<div>You got ${game.score} correct vs ${game.incorrect} incorrect out of ${game.played} words total.</div><br>`;
         btn.style.display = 'block';
         output.innerHTML += "Please Select your word list";
         selWordList.style.display = 'block';
+        btn.textContent = 'Start New Game';
+        inWord.value = '';
+        game.score = 0;
+        game.incorrect = 0;
     } else {
         inWord.disabled = false;
         inWord.value = '';
@@ -85,6 +98,7 @@ btn.addEventListener('click', (e) => {
         btn.style.display = 'none';
         myWords.sort(() => { return 0.5 - Math.random(); });
         game.sel = myWords.shift();
+        tempWord = game.sel;
         game.wordsLeft = myWords.length;
         game.scramble = sorter(game.sel);
         addScore();
@@ -106,28 +120,40 @@ inWord.addEventListener('keyup', (e) => {
 
 function addScore() {
     let tempOutput = `Score: <b>${game.score}</b> vs incorrect <i>(${game.incorrect})</i> <small>${game.wordsLeft}  words left</small>`;
-    scoreBoard.innerHTML = tempOutput;
+    scoreBoard.innerHTML = tempOutput;    
 }
 
 function winChecker() {
     inWord.style.borderWidth = '5px';
     if (inWord.value === game.sel) {
-                inWord.style.borderColor = 'green';
+        inWord.style.borderColor = 'green';
         game.score++;
         inWord.disabled = true;
-        btn.style.display ='block';
-        btn.textContent = 'Click for next word';
+        btn.style.display = 'block';
+        if (myWords.length > 0) {
+            btn.textContent = 'Click for next word';
+        } else {
+            btn.textContent = 'Click for final score';
+        }        
     } else {
         inWord.style.borderColor = 'red';
-        inWord.value = '';
         inWord.focus();
         game.incorrect++;
+        output.innerHTML = `The correct answer is: ${tempWord}`;
+        inWord.disabled = true;
+        btn.style.display ='block';
+        if (myWords.length > 0) {
+            btn.textContent = 'Click for next word';
+        } else {
+            btn.textContent = 'Click for final score';
+        }
+        tempWord = "";
     }
     addScore();
 }
 
 function sorter(val) {
-   let temp = game.sel.split('');    
+   let temp = val.split('');    
    temp.sort(()=>{ return 0.5 - Math.random(); });
    temp = temp.join('');   
     if(val === temp) {       
